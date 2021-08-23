bdb3adc60ab2f31e2bd0383454995e3596585a64
jsssj <eeun45@gmail.com>
2021-08-23 15:13:55 +0900

    8/23

--- projectapp/models.py
+++ projectapp/models.py
@@ -5,7 +5,8 @@ from django.db import models
 class Project(models.Model):
     name = models.CharField(max_length=20, null=False)
     description = models.CharField(max_length=200, null=False)
-    image = models.ImageField(upload_to='project/', null=False)
+    # upload_to 하면 media 폴더에 해당 폴더 생김
+    image =  models.ImageField(upload_to='project/', null=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
 
